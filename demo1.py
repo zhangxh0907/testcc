63c8d179196a197ff5b54567fca4658586995b86
zhangxh <zxh20111204@126.com>
2020-08-29 01:03:23 +0800

    第一次提交练习作业

--- demo1.py
+++ demo1.py
@@ -1,1 +1,11 @@
-print("hello world!")
+# print("hello world!")
+
+
+a = len(input("请输入你想输入的内容："))
+print(a)
+b = a%2
+print(b)
+if b == 0:
+    print("您输入的字数是双数")
+if b == 1:
+    print("您输入的字数是单数")
